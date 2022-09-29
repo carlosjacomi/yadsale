4e4788ca3af9d213edf8912e19c15f0b6f4887b4
Carlos Tajibo <ccontreras@tajibo.com>
2022-09-28 23:03:27 -0300

    Se unio el menu mobil y se aplicaron los CSS junto al responsive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuDesk = document.querySelector('.desktop-menu');
+const menuIco = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',toggDesktopMenu);
 
 function toggDesktopMenu() {
 	menuDesk.classList.toggle('inactive');
+}
+
+menuIco.addEventListener('click',toggMobileMenu);
+
+function toggMobileMenu() {
+	menuMobile.classList.toggle('inactive');
 }
