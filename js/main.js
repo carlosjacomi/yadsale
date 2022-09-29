0de62f6ed77088e5f318e0e424f19a069a4e6022
Carlos Tajibo <ccontreras@tajibo.com>
2022-09-28 23:25:09 -0300

    Se agrego el carrito de compras y se modifico el css

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ const menuEmail = document.querySelector('.navbar-email');
 const menuDesk = document.querySelector('.desktop-menu');
 const menuIco = document.querySelector('.menu');
 const menuMobile = document.querySelector('.mobile-menu');
+const carritoIco = document.querySelector('.navbar-shopping-cart');
+const flechaCarrito = document.querySelector('.flecha-carrito');
+const detalleCarrito = document.querySelector('.carrito');
+
 
 menuEmail.addEventListener('click',toggDesktopMenu);
 
@@ -13,4 +17,12 @@ menuIco.addEventListener('click',toggMobileMenu);
 
 function toggMobileMenu() {
 	menuMobile.classList.toggle('inactive');
+}
+
+carritoIco.addEventListener('click',toggCarrito);
+flechaCarrito.addEventListener('click',toggCarrito);
+
+
+function toggCarrito() {
+	detalleCarrito.classList.toggle('inactive');
 }
